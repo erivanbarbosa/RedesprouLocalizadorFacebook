96a03e9fc014c27d0919499a202ba47fd7e3c6bc
Erivan <erivan.nas@gmail.com>
2017-06-07 12:41:02 -0300

    Atualiza MainActivity
    Adiciona método initViews para iniciar as variáveis com seus componentes View adequados.

--- app/src/main/java/com/redesprou/redesproulocalizadorfacebook/MainActivity.java
+++ app/src/main/java/com/redesprou/redesproulocalizadorfacebook/MainActivity.java
@@ -14,11 +14,18 @@ public class MainActivity extends AppCompatActivity {
     Button searchButton;
     EditText searchEditText;
     Spinner numberOfResultsSpinner;
-    
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void initViews(){
+        loginButton = (LoginButton) findViewById(R.id.login_button);
+        searchButton = (Button) findViewById(R.id.search_button);
+        searchEditText = (EditText) findViewById(R.id.search_EditText);
+        numberOfResultsSpinner = (Spinner) findViewById(R.id.search_Spinner );
     }
 }

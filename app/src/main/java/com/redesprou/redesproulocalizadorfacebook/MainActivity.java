b4f36ac8f7b49bd5fec45ab698252469956f855a
Erivan <erivan.nas@gmail.com>
2017-06-07 12:25:25 -0300

    Atualiza MainActivity
    Adicionadas variáveis para cada elemento do layout correspondente.

--- app/src/main/java/com/redesprou/redesproulocalizadorfacebook/MainActivity.java
+++ app/src/main/java/com/redesprou/redesproulocalizadorfacebook/MainActivity.java
@@ -2,8 +2,19 @@ package com.redesprou.redesproulocalizadorfacebook;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
+
+import com.facebook.login.widget.LoginButton;
 
 public class MainActivity extends AppCompatActivity {
+
+    LoginButton loginButton;
+    Button searchButton;
+    EditText searchEditText;
+    Spinner numberOfResultsSpinner;
+    
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {

fbd188dac2ed31d19565fdb3984075fd04d4c0ed
Erivan <erivan.nas@gmail.com>
2017-07-02 15:22:02 -0300

    Insere milhares de configurações enfadonhas exigidas pelo facebook.

--- app/src/main/java/com/redesprou/redesproulocalizadorfacebook/MainActivity.java
+++ app/src/main/java/com/redesprou/redesproulocalizadorfacebook/MainActivity.java
@@ -1,28 +1,46 @@
 package com.redesprou.redesproulocalizadorfacebook;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.support.v4.app.FragmentActivity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.telecom.Call;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Spinner;
+import android.widget.Toast;
 
 import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
 import com.facebook.FacebookSdk;
+import com.facebook.login.LoginManager;
+import com.facebook.login.LoginResult;
 import com.facebook.login.widget.LoginButton;
+
+import com.facebook.FacebookSdk;
+import com.facebook.appevents.AppEventsLogger;
+
 
 import java.util.Arrays;
 import java.util.List;
 
-public class MainActivity extends AppCompatActivity {
+import static com.facebook.internal.FacebookDialogFragment.TAG;
+
+public class MainActivity extends AppCompatActivity  {
 
     LoginButton loginButton;
     Button searchButton;
     EditText searchEditText;
     Spinner numberOfResultsSpinner;
-    CallbackManager callBackManager;
+    public static CallbackManager callbackManager;
     RecyclerView recyclerView;
 
 
@@ -30,22 +48,48 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        callBackManager = CallbackManager.Factory.create();
-        List<String> permitions = Arrays.asList("email", "public_profile", "user_friends", "user_location", "user_likes");
-        loginButton.setReadPermissions( permitions );
         recyclerView = (RecyclerView) findViewById(R.id.recyclerView );
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
 
         final ItemAdapter itemAdapter = new ItemAdapter();
+        callbackManager = CallbackManager.Factory.create();
+
+        initViews();
+        List<String> permitions = Arrays.asList("email", "public_profile", "user_friends", "user_location", "user_likes");
+        loginButton.setReadPermissions( permitions );
+
+        LoginManager.getInstance().registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
+            @Override
+            public void onSuccess(LoginResult loginResult) {
+                Toast.makeText(getApplicationContext(), "Logged in successfully!", Toast.LENGTH_SHORT).show();
+                Log.i(TAG, "Facebook login successful.");
+            }
+
+            @Override
+            public void onCancel() {
+                Toast.makeText(getApplicationContext(), "Logging in canceled.", Toast.LENGTH_SHORT).show();
+                Log.i(TAG, "Facebook login canceled.");
+            }
+
+            @Override
+            public void onError(FacebookException error) {
+                Toast.makeText(getApplicationContext(), "Error occurred while logging in. Please try again.", Toast.LENGTH_SHORT).show();
+                Log.i(TAG, "Facebook login error.");
+            }
+        });
     }
+
 
     public void initViews(){
         loginButton = (LoginButton) findViewById(R.id.login_button);
         searchButton = (Button) findViewById(R.id.search_button);
         searchEditText = (EditText) findViewById(R.id.search_EditText);
         numberOfResultsSpinner = (Spinner) findViewById(R.id.search_Spinner );
-
-
-
     }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        callbackManager.onActivityResult(requestCode, resultCode, data);
+    }
+
 }

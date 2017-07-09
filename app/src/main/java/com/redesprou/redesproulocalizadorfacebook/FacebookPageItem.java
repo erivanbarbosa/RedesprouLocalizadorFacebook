3a7d1b747c3d662e9e4b9a486b0dc707bbf9a4bc
Romulo Borges Crispi <fulanux@gmail.com>
2017-07-09 15:14:47 -0300

    Adição de evidenciador de item da página
    Por meio desta entrada, a aplicação é capaz de evidenciar as informações recolhidas da API do facebook

---
+++ app/src/main/java/com/redesprou/redesproulocalizadorfacebook/FacebookPageItem.java
@@ -0,0 +1,27 @@
+package com.redesprou.redesproulocalizadorfacebook;
+
+/**
+ * Created by Fulanuc on 09/07/2017.
+ */
+
+public class FacebookPageItem {
+
+    private String nomePagina;
+    private String descricaoPagina;
+
+    public String getNomePagina() {
+        return nomePagina;
+    }
+
+    public void setNomePagina(String nomePagina) {
+        this.nomePagina = nomePagina;
+    }
+
+    public String getDescricaoPagina() {
+        return descricaoPagina;
+    }
+
+    public void setDescricaoPagina(String descricaoPagina) {
+        this.descricaoPagina = descricaoPagina;
+    }
+}
